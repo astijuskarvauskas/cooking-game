0bedba29df1ab92b357baf45548a713dcd822462
Astijus Karvauskas <astijusk7@gmail.com>
2026-03-17 21:14:36 +0000

    Grid constructor now instantiates each Tile with parameters

--- grid.py
+++ grid.py
@@ -1,8 +1,6 @@
-import tile
 from tile import Tile
 
 class Grid:
     def __init__(self, size):
         self.size = size
-        self.tileMap = [[Tile() for _ in range(size+1)] for _ in range(size+1)]
-
+        self.tileMap = [[Tile(x * Tile.tileSize, y * Tile.tileSize) for x in range(size+1)] for y in range(size+1)]
