95f3c791d7b90dd04ef95128ef898a17306aeb3b
Astijus Karvauskas <astijusk7@gmail.com>
2026-03-17 21:13:28 +0000

    Tiles now rended based on 'selected' state. Seperated rendering of Tile from 'draw_map' func

--- renderer.py
+++ renderer.py
@@ -1,5 +1,7 @@
 import pygame
 
+from tile import Tile
+
 class Renderer:
     def __init__(self, surface, dt):
         self.surface = surface
@@ -8,17 +10,33 @@ class Renderer:
     def draw_player(self, player):
         pygame.draw.circle(self.surface, player.color, (player.x, player.y), player._radius) #np.
 
+    def draw_tile(self, tile):
+        default_color = (255, 255, 255)
+        selected_color = (255, 0, 0)
+
+        if tile.selected:
+            pygame.draw.rect(
+                self.surface,
+                selected_color,
+                pygame.Rect(
+                    tile.x,
+                    tile.y,
+                    Tile.tileSize,
+                    Tile.tileSize
+                ), 1)
+        else:
+            pygame.draw.rect(
+                self.surface,
+                default_color,
+                pygame.Rect(
+                    tile.x,
+                    tile.y,
+                    Tile.tileSize,
+                    Tile.tileSize
+                ), 1)
+
     def draw_map(self, grid):
         for row in range(len(grid.tileMap)):
             for col in range(len(grid.tileMap[row])):
-                tile_x, tile_y = col, row
-                pygame.draw.rect(
-                    self.surface,
-                    (255, 255, 255),
-                    pygame.Rect(
-                        row * 80,
-                        col * 80,
-                        80,
-                        80
-                    ),
-                    1)
+                # tile_x, tile_y = col, row
+                self.draw_tile(grid.tileMap[row][col])
